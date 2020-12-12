b531c5df8a2875c700674f5af8dd95234fb1d8e4
Cmac <mckoyc@gmail.com>
2020-12-12 12:11:26 -0500

    Created new model for User List and associated User with List

---
+++ models/list.js
@@ -0,0 +1,32 @@
+const { sequelize } = require(".");
+
+module.exports = (sequelize, Datatypes) => {
+    const List = sequelize.define("User_List", {
+        // Game Title can't be Null
+        title: {
+            type: Datatypes.STRING,
+            allowNull: false,
+        },
+        // Must choose a game status of "Completed, Currently Playing, Dropped, Want to Play" 
+        status: {
+            type: Datatypes.STRING,
+            allowNull: false
+        },
+        // Multiplayer or Single Player
+        type: Datatypes.STRING,
+        // Optional Categories
+        hours_played: Datatypes.INTEGER,
+        rating: Datatypes.INTEGER,
+    });
+
+    List.associate = (models) => {
+        // Must be User attached to List
+        List.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: false
+            }
+        })
+    }
+
+    return List;
+}
