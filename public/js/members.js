0dfcbeeebbdd9728aa97757660667360a7567fbc
Cmac <mckoyc@gmail.com>
2020-12-16 21:37:56 -0500

    added member id to local storage and linked js files to html pages

--- public/js/members.js
+++ public/js/members.js
@@ -2,6 +2,8 @@ $(document).ready(() => {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(data => {
+    let id = data.id
     $(".member-name").text(data.email);
+    localStorage.setItem("user", id)
   });
 });
