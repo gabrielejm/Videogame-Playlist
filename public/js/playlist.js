dde135d7e99c291b986dac104074f1ef058ba9a8
Cmac <mckoyc@gmail.com>
2020-12-18 16:03:35 -0500

    added skeleton function of renderlist to playlist.js

--- public/js/playlist.js
+++ public/js/playlist.js
@@ -3,25 +3,52 @@ $(document).ready(()=>{
     const addHoursPlayed = $("")
     const addType = $("")
     const updateGame = $("")
+    const designatedList = $("")
+    const pickListBtn = $("")
     const user = localStorage.getItem("user")
-
+    
     renderList = () => {
         $.get(`/api/user_data/${user}`)
         .then(data => {
             for (let i = 0; i < data.length; i++){
-                let gamesList = $("#example")
-                let title = $("<h1>")
-                let status = $("<h2>")
-
-
+                console.log(data[i])
+                let gamesList = $(".table-body")
+                let newRow = $("<tr>")
+                let title = $("<td>")
+                let status = $("<td>")
+                let type = $("<td>")
+                let hours = $("<td>")
+                let ratings = $("<td>")
+                
+                
                 title.text(data[i].title)
                 status.text(data[i].status)
+                type.text(data[i].type)
+                hours.text(data[i].hoursPlayed)
+                ratings.text(data[i].rating)
 
-                gamesList.append(title)
-                gamesList.append(status)
+                newRow.append(title)
+                newRow.append(status)
+                newRow.append(type)
+                newRow.append(hours)
+                newRow.append(ratings)
+
+                gamesList.append(newRow)
             }
         })
     }
-
+    
+    const renderSpecified = () => {
+        
+        $.get(`/api/user_data/${user}/`)
+        .then(data => {
+            for (let i = 0; i < data.length; i++){
+                
+            }
+        })
+    }
+    
+    
+    pickListBtn.on("click", renderSpecified)
     renderList() 
 })
