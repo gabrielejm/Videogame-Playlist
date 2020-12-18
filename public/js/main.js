233f0ffebd5f37ea56f198bc2b7e385540d39969
Cmac <mckoyc@gmail.com>
2020-12-18 15:06:06 -0500

    wrote beginner code for ajax call for search button

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,8 @@
 $(document).ready(() => {
     const searchBtn = $("#run-search")
     const addBtn = $("")
-    const game = $("#game-search").val()
     const user = localStorage.getItem("user")
-
+    
     addBtn.on("click", () => {
         console.log("Button clicked")
         $.post(`/api/user_data/list/${user}`)
@@ -11,10 +10,11 @@ $(document).ready(() => {
             alert(`${game} added to List`)
         })
     })
-
+    
     searchBtn.on("click", function(event) {
         event.preventDefault();
         //get value of the input box when you click run search
+        const game = $("#search-game").val().split(" ").join("-")
         let APIKey = "830ae949e6554aadae74c234f1e467ee";
         let queryUrl = `https://api.rawg.io/api/games?key=${APIKey}&search=${game}`;
         
@@ -23,7 +23,12 @@ $(document).ready(() => {
         url: queryUrl
         }).then( function(data){
             console.log("game: ", game)
-            console.log("data: ", data)         
+            console.log("data: ", data.results[0].name)
+            for (let i = 0; i < 5; i++){
+                let title = data.results[i].name
+                let released = data.results[i].released
+                let rating = `${data.results[i].rating} / 5`
+            }         
         })
     })
 })
