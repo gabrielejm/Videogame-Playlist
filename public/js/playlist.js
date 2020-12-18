8d4ac29ab036fc2fdaecb2dead5a003c48c52b57
Cmac <mckoyc@gmail.com>
2020-12-18 17:04:40 -0500

    updated search basics to only return a single result | added semantics to renderList function

--- public/js/playlist.js
+++ public/js/playlist.js
@@ -11,28 +11,40 @@ $(document).ready(()=>{
         $.get(`/api/user_data/${user}`)
         .then(data => {
             for (let i = 0; i < data.length; i++){
-                console.log(data[i])
                 let gamesList = $(".table-body")
+                let input = $("<input>")
+                //Appending Row variables
                 let newRow = $("<tr>")
                 let title = $("<td>")
                 let status = $("<td>")
                 let type = $("<td>")
                 let hours = $("<td>")
                 let ratings = $("<td>")
-                
+                let buttons = $("<td>")
+                let deleteBtn = $("<button>").text("Delete")
+                let updateBtn = $("<button>").text("Update")
+
+                input.val(data[i].title)
                 
                 title.text(data[i].title)
                 status.text(data[i].status)
                 type.text(data[i].type)
-                hours.text(data[i].hoursPlayed)
+                // hours.text(data[i].hoursPlayed)
+                hours.append(input)
+                hours.append("hours")
                 ratings.text(data[i].rating)
+                buttons.append(updateBtn)
+                buttons.append(deleteBtn)
 
+                //Appends Game Data to Row
                 newRow.append(title)
                 newRow.append(status)
                 newRow.append(type)
                 newRow.append(hours)
                 newRow.append(ratings)
+                newRow.append(buttons)
 
+                //Appends Row to Page
                 gamesList.append(newRow)
             }
         })
